890d10b4667d0dccf839ef2cc64b90705fc825d7
polterguy <thomas@magixilluminate.com>
2014-10-28 11:24:22 +0100

    - changed copyright notice to become "Gaia"

--- phosphorus.five.samples/Literal.aspx.cs
+++ phosphorus.five.samples/Literal.aspx.cs
@@ -1,5 +1,5 @@
 /*
- * phosphorus five, copyright 2014 - thomas@magixilluminate.com
+ * phosphorus five, copyright 2014 - Mother Earth, Jannah, Gaia
  * phosphorus five is licensed as mitx11, see the enclosed LICENSE file for details
  */
 
