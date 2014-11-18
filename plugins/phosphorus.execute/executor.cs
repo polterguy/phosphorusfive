0ab66e6660491536a1e9088f7682f14a4db65d3d
polterguy <thomas@magixilluminate.com>
2014-11-18 02:28:04 +0100

    - storing execution instruction pointer during execution

--- plugins/phosphorus.execute/executor.cs
+++ plugins/phosphorus.execute/executor.cs
@@ -15,19 +15,36 @@ namespace phosphorus.execute
     public static class executor
     {
         /// <summary>
-        /// main execution Active Event entry point for executing nodes as execution trees
+        /// main execution Active Event entry point for executing nodes as execution tree
         /// </summary>
         /// <param name="context"><see cref="phosphorus.Core.ApplicationContext"/> for Active Event</param>
         /// <param name="e">parameters passed into Active Event</param>
         [ActiveEvent (Name = "pf.execute")]
         private static void pf_execute (ApplicationContext context, ActiveEventArgs e)
         {
-            Node current = e.Args.FirstChild;
-            while (current != null) {
-                if (!current.Name.StartsWith ("_"))
+            Node ip = new Node ("__pf_ip", null);
+            e.Args.Root.Add (ip);
+            if (e.Args.Count == 0 || !Expression.IsExpression (e.Args.FirstChild.Get<string> ())) {
+
+                // executing current scope
+                Node current = e.Args.FirstChild;
+                while (current != null) {
+                    if (!current.Name.StartsWith ("_")) {
+                        ip.Value = current.Path;
+                        context.Raise (current.Name, current);
+                    }
+                    current = current.NextSibling;
+                }
+            } else {
+
+                // executing expression
+                Match executionMatch = new Expression (e.Args.FirstChild.Get<string> ()).Evaluate (e.Args.FirstChild);
+                foreach (Node current in executionMatch.Matches) {
+                    ip.Value = current.Path;
                     context.Raise (current.Name, current);
-                current = current.NextSibling;
+                }
             }
+            ip.Untie ();
         }
     }
 }
