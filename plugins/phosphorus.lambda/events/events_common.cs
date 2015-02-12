f253338526d98e605da926a43685a8e73ab8b04e
polterguy <thomas@magixilluminate.com>
2015-02-12 19:00:50 +0100

    - still refactoring

--- plugins/phosphorus.lambda/events/events_common.cs
+++ plugins/phosphorus.lambda/events/events_common.cs
@@ -19,6 +19,14 @@ namespace phosphorus.lambda
         // contains our list of dynamically created Active Events
         private static Dictionary<string, Node> _events = new Dictionary<string, Node> ();
 
+        // used to create lock when creating, deleting and consuming events
+        private static object _lock;
+
+        static events_common ()
+        {
+            _lock = new object ();
+        }
+
         /// <summary>
         /// creates a dynamic Active Event
         /// </summary>
@@ -26,14 +34,19 @@ namespace phosphorus.lambda
         /// <param name="lambdas">lambda objects</param>
         public static void CreateEvent (string name, IEnumerable<Node> lambdas)
         {
-            // making sure we have a key for Active Event name
-            if (!_events.ContainsKey (name))
-                _events [name] = new Node ();
+            // acquiring lock, in case multiple threads creates the same event, or event is consumed by another
+            // thread simultaneously
+            lock (_lock) {
 
-            // looping through each "lambda.xxx" node inside of event creation node, appending these
-            // into our event node
-            foreach (Node idxLambda in lambdas) {
-                _events [name].Add (idxLambda.Clone ());
+                // making sure we have a key for Active Event name
+                if (!_events.ContainsKey (name))
+                    _events [name] = new Node ();
+
+                // looping through each "lambda.xxx" node inside of event creation node, appending these
+                // into our event node
+                foreach (Node idxLambda in lambdas) {
+                    _events [name].Add (idxLambda.Clone ());
+                }
             }
         }
 
@@ -43,8 +56,13 @@ namespace phosphorus.lambda
         /// <param name="name">name of Active Event(s) to delete</param>
         public static void DeleteEvent (string name)
         {
-            if (_events.ContainsKey (name))
-                _events.Remove (name);
+            // acquiring lock
+            lock (_lock) {
+
+                // removing event, if it exists
+                if (_events.ContainsKey (name))
+                    _events.Remove (name);
+            }
         }
 
         /*
@@ -55,10 +73,27 @@ namespace phosphorus.lambda
         {
             if (_events.ContainsKey (e.Name)) {
 
-                // looping through all [lambda.xxx] objects in event node, concatenating these into
-                // event invocation statement, before we execute event invocation node
-                e.Args.AddRange (_events [e.Name].Clone ().Children);
-                context.Raise ("lambda", e.Args);
+                // used to signal whether or not we should actually raise the event
+                bool shouldRaise = false;
+
+                // acquiring lock
+                lock (_lock) {
+
+                    // then re-checking after lock is acquired, to make sure event is still around
+                    if (_events.ContainsKey (e.Name)) {
+
+                        // looping through all [lambda.xxx] objects in event node, concatenating these into
+                        // event invocation statement, before we release lock, and execute event invocation node
+                        e.Args.AddRange (_events [e.Name].Clone ().Children);
+
+                        // signaling to outer parts that we should raise event
+                        shouldRaise = true;
+                    }
+                }
+
+                // in case event was deleted efter first check, but before nodes were appended
+                if (shouldRaise)
+                    context.Raise ("lambda", e.Args);
             }
         }
     }
