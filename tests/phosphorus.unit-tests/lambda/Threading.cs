6e9e1222304683daeacc50d5a16ada891e08613b
polterguy <thomas@magixilluminate.com>
2015-02-20 01:56:23 +0100

    - threading unit tests initiated

---
+++ tests/phosphorus.unit-tests/lambda/Threading.cs
@@ -0,0 +1,48 @@
+
+/*
+ * phosphorus five, copyright 2014 - Mother Earth, Jannah, Gaia
+ * phosphorus five is licensed as mit, see the enclosed LICENSE file for details
+ */
+
+using System;
+using System.IO;
+using NUnit.Framework;
+using phosphorus.core;
+
+namespace phosphorus.unittests.lambda
+{
+    /// <summary>
+    /// unit tests for testing the threading features of pf.lambda
+    /// </summary>
+    [TestFixture]
+    public class Threading : TestBase
+    {
+        public Threading ()
+            : base ("phosphorus.file", "phosphorus.types", "phosphorus.hyperlisp", "phosphorus.threading")
+        { }
+
+        /// <summary>
+        /// forks [multiple] threads using [lambda.fork], using a [wait] and a [lock] inside of thread lambda,
+        /// making sure all threads are capable of returning values through the [_wait] reference node
+        /// </summary>
+        [Test]
+        public void Threading01 ()
+        {
+            Node node = ExecuteLambda (@"_exe
+  lock:job
+    append:@/././*/_wait/#/*/_foos?node
+      source:@/../*/""/foo/""?node
+wait
+  lambda.fork:@/../*/_exe?node
+    foo1:bar1
+  lambda.fork:@/../*/_exe?node
+    foo2:bar2
+  lambda.fork:@/../*/_exe?node
+    foo3:bar3
+  lambda.fork:@/../*/_exe?node
+    foo4:bar4
+  _foos");
+            Assert.AreEqual (4, node [1] [4].Count);
+        }
+    }
+}
