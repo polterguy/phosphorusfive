cb6c8918de253f28f1a3f7c76237003c52906fd0
polterguy <thomas@magixilluminate.com>
2015-02-28 14:54:37 +0100

    - immediate mode done in lambda.exe

--- core/phosphorus.exe/Program.cs
+++ core/phosphorus.exe/Program.cs
@@ -4,6 +4,7 @@
  */
 
 using System;
+using System.Text;
 using System.Reflection;
 using phosphorus.core;
 using phosphorus.expressions;
@@ -65,8 +66,11 @@ namespace phosphorus.exe
         {
             try {
                 if (args == null || args.Length == 0) {
-                    OutputInstructions (); // outputting instructions, and then exiting
+
+                    // outputting instructions, and then exiting
+                    OutputInstructions ();
                 } else {
+
                     // initializing plugins that must be here in order for lambda executioner to function
                     Loader.Instance.LoadAssembly (Assembly.GetExecutingAssembly ());
                     Loader.Instance.LoadAssembly ("plugins/", "phosphorus.hyperlisp");
@@ -74,8 +78,8 @@ namespace phosphorus.exe
                     Loader.Instance.LoadAssembly ("plugins/", "phosphorus.file");
 
                     // handling our command-line arguments
-                    string language;
-                    var exeNode = ParseArguments (args, out language);
+                    bool immediate;
+                    var exeNode = ParseArguments (args, out immediate);
 
                     // creating application context after parameters are loaded, since there might be
                     // additional plugins requested during the parsing of our command-line arguments
@@ -84,19 +88,62 @@ namespace phosphorus.exe
                     // raising our application startup Active Event, in case there are modules loaded depending upon it
                     context.Raise ("pf.core.application-start", new Node ());
 
-                    // loads and convert file to lambda nodes
-                    var convertExeFile = context.Raise ("pf.hyperlisp.hyperlisp2lambda", new Node (string.Empty,
-                        context.Raise ("pf.file.load", new Node (string.Empty, exeNode.Value)) [0].Get<string> (context)));
+                    if (immediate) {
 
-                    // appending nodes from lambda file into execution objects, and execute lambda file given through command-line arguments
-                    exeNode.AddRange (convertExeFile.Children);
-                    context.Raise ("lambda", exeNode);
+                        // starting immediate mode
+                        ImmediateMode (context);
+                    } else {
+
+                        // loads and convert file to lambda nodes
+                        var convertExeFile = 
+                            context.Raise ("pf.hyperlisp.hyperlisp2lambda", 
+                                           new Node (string.Empty, 
+                                           context.Raise ("pf.file.load", 
+                                               new Node (string.Empty, exeNode.Value)) [0].Get<string> (context)));
+
+                        // appending nodes from lambda file into execution objects, and execute lambda file given through command-line arguments
+                        exeNode.AddRange (convertExeFile.Children);
+                        context.Raise ("lambda", exeNode);
+                    }
                 }
             } catch (Exception err) {
                 while (err.InnerException != null)
                     err = err.InnerException;
                 Console.WriteLine (err.Message);
                 Console.WriteLine (err.StackTrace);
+            }
+        }
+
+        /*
+         * starts immediate mode, allowing user to type in a bunch of Hyperlisp, executing when empty line is submitted
+         */ 
+        private static void ImmediateMode (ApplicationContext context)
+        {
+            while (true) {
+                StringBuilder builder = new StringBuilder ();
+                while (true) {
+                    Console.Write("pf>");
+                    string line = Console.ReadLine ();
+                    if (line == string.Empty)
+                        break;
+                    if (line == "exit") {
+
+                        // discarding input and signaling exit of outer loop
+                        builder = new StringBuilder ("exit");
+                        break;
+                    }
+                    builder.Append (line + "\r\n");
+                }
+                string hyperlisp = builder.ToString ();
+                if (hyperlisp == "exit") {
+                    break;
+                } else if (hyperlisp.Trim () == string.Empty) {
+                    Console.WriteLine ("nothing to do here");
+                } else {
+                    Node convert = context.Raise ("pf.hyperlisp.hyperlisp2lambda", new Node (string.Empty, hyperlisp));
+                    context.Raise ("lambda", convert);
+                    Console.WriteLine ();
+                }
             }
         }
 
@@ -118,6 +165,9 @@ namespace phosphorus.exe
             Console.WriteLine ("-p allows you to load additional plugins, for instance; -p \"plugins/my.plugin\"");
             Console.WriteLine ("   you can repeat the -p argument as many times as you wish");
             Console.WriteLine ();
+            Console.WriteLine ("-i starts 'immediate mode', allowing you to write in any Hyperlisp, ending and");
+            Console.WriteLine ("executing your code with an empty line. End immediate mode with 'exit'");
+            Console.WriteLine ();
             Console.WriteLine ("All other arguments are passed into the execution tree of the lambda file you "
                                + "are executing as a key/value pair, e.g; _var \"x\" creates a new node for you "
                                + "at the top of your execution file called '_var' with the content of 'x'");
@@ -131,9 +181,9 @@ namespace phosphorus.exe
         /*
          * creates our node parameter collection to pass into pf.lambda execution engine
          */
-        private static Node ParseArguments (string[] args, out string language)
+        private static Node ParseArguments (string[] args, out bool immediate)
         {
-            language = null;
+            immediate = false;
             var exeNode = new Node ("input-file");
             var nextIsInput = false;
             var nextIsPlugin = false;
@@ -150,6 +200,8 @@ namespace phosphorus.exe
                     nextIsPlugin = false;
                 } else if (idx == "-p") {
                     nextIsPlugin = true;
+                } else if (idx == "-i") {
+                    immediate = true;
                 } else if (exeNode.Count == 0 || exeNode [exeNode.Count - 1].Value != null) {
                     exeNode.Add (new Node (idx));
                 } else {
@@ -157,8 +209,8 @@ namespace phosphorus.exe
                 }
             }
 
-            if (exeNode.Value == null)
-                throw new ArgumentException ("No execution file given to lambda executor");
+            if (exeNode.Value == null && !immediate)
+                throw new ArgumentException ("No execution file given to lambda executor, neither was immediate mode chosen");
             return exeNode;
         }
     }
