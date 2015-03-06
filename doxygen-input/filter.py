caefbb0563df1c4c5988e4e25450dc3160157054
polterguy <thomas@magixilluminate.com>
2015-03-06 14:32:02 +0100

    - custom headers and such for doxygen

---
+++ doxygen-input/filter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys
+import re
+
+if (len(sys.argv) < 2):
+    print "No input file"
+else:
+    f = open(sys.argv[1])
+    line = f.readline()
+    while line:
+        re1 = re.compile("\[ActiveEvent\s*\(\s*Name\s*=\s*\"(.*)\"\)]")
+        re1.search(line)
+        sys.stdout.write(re1.sub(r"/// Active Event: \"\1\" \\ingroup ActiveEvents\n", line))
+        #sys.stdout.write(line)
+        line = f.readline()
+    f.close()
