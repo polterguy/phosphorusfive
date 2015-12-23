1f4796292300dc3805ad6c963d421d79bdaded34
Thomas Hansen <phosphorusfive@gmail.com>
2015-12-23 22:12:38 +0100

    foo
    bar

--- core/p5.exp/iterators/IteratorFlatten.cs
+++ core/p5.exp/iterators/IteratorFlatten.cs
@@ -18,12 +18,18 @@ namespace p5.exp.iterators
         public override IEnumerable<Node> Evaluate (ApplicationContext context)
         {
             foreach (var idxCurrent in Left.Evaluate (context)) {
+
+                // If next sibling is not null, then that is our "stop node"
                 var stop = idxCurrent.NextSibling;
+
+                // Looping until stop not equals null, or we have no more nodes to iterate, because we're at root
                 var curIdx = idxCurrent.Parent;
                 while (stop == null && curIdx != null) {
-                    stop = curIdx.NextNode;
+                    stop = curIdx.NextSibling;
                     curIdx = curIdx.Parent;
                 }
+
+                // At this point, stop is either node where to stop iterator, or null, at which point we iterate until null is reached
                 for (var idxNext = idxCurrent; idxNext != null && !idxNext.Equals (stop); idxNext = idxNext.NextNode) {
                     yield return idxNext;
                 }
--- plugins/extras/p5.mime/plugins/HttpRequest.cs
+++ plugins/extras/p5.mime/plugins/HttpRequest.cs
@@ -26,6 +26,7 @@ namespace p5.mime.plugins
         /// </summary>
         [ActiveEvent (Name = "p5.net.http-post-mime", Protection = EventProtection.LambdaClosed)]
         [ActiveEvent (Name = "p5.net.http-put-mime", Protection = EventProtection.LambdaClosed)]
+        [ActiveEvent (Name = "p5.net.http-get-mime", Protection = EventProtection.LambdaClosed)]
         public static void p5_net_http_post_put_mime (ApplicationContext context, ActiveEventArgs e)
         {
             // Making sure we clean up after ourselves
@@ -36,18 +37,30 @@ namespace p5.mime.plugins
                 if (decryptionKeys != null)
                     decryptionKeys.UnTie (); // Don't want passwords to leave method in case of exceptions
 
+                // Storing attachment folder, to know where to serialise returned attachments, of there is one
+                var attachmentFolder = e.Args ["attachment-folder"];
+                if (attachmentFolder != null)
+                    attachmentFolder.UnTie (); // Don't want to confuse creation of MimeEntity
+
+                // Retrieving all HTTP headers, which are defined as children with Capital letters, and untying them,
+                // to not confuse creation of MimeEntity
+                var headerList = (from x in e.Args.Children where x.Name != x.Name.ToLower () select x).ToList ();
+                foreach (var idxHeader in headerList) {
+                    idxHeader.UnTie ();
+                }
+
                 // Figuring out which HTTP method to use
                 string method = e.Args.Name.Substring (e.Args.Name.IndexOf ("-") + 1);
                 method = method.Substring (0, method.IndexOf ("-"));
 
-                // Creating request MimeEntity
-                var requestEntity = CreateEntity (context, e.Args);
+                // Creating request MimeEntity, but not if method is GET
+                var requestEntity = method == "get" ? null : CreateEntity (context, e.Args);
 
                 // Creating HTTP request(s), and retrieving result
-                var result = CreateHttpRequest (context, e.Args, requestEntity, method);
+                var result = CreateHttpRequest (context, e.Args, requestEntity, method, headerList);
 
                 // Parsing response as MIME and returning to caller
-                ParseResponse (context, e.Args, result, decryptionKeys);
+                ParseResponse (context, e.Args, result, decryptionKeys, attachmentFolder);
             }
         }
 
@@ -65,7 +78,7 @@ namespace p5.mime.plugins
                 var createMimeNode = new Node ("", bufferStreams);
 
                 // Removing [decryption-keys] to not confuse mime creator
-                createMimeNode.AddRange (args.Children.Where (ix => ix.Name != "decryption-keys"));
+                createMimeNode.AddRange (args.Children);
 
                 // Returning MimeEntity to caller
                 return context.RaiseNative ("p5.mime.create-native", createMimeNode).Get<MimeEntity> (context);
@@ -88,19 +101,29 @@ namespace p5.mime.plugins
             ApplicationContext context, 
             Node args, 
             MimeEntity entity, 
-            string method)
+            string method,
+            IEnumerable<Node> headerList)
         {
             // Figuring out Content-Type HTTP header to use, and creating Node to use for HTTP request invocation Active Event
-            var contentType = entity.ContentType.MediaType + "/" + entity.ContentType.MediaSubtype + entity.ContentType.Parameters;
             var activeEventName = string.Format ("p5.net.http-{0}", method);
             var requestNode = new Node (activeEventName, args.Value);
 
-            // Streaming MimeEntity to memory, before invoking Active Event that creates HTTP request
-            using (var stream = new MemoryStream()) {
-                entity.WriteTo (stream, true);
+            // Streaming MimeEntity to memory, if we have one, before invoking Active Event that creates HTTP request
+            if (entity != null) {
+
+                // We have an entity to post in our HTTP request, making sure we get the Content-Type right
+                var contentType = entity.ContentType.MediaType + "/" + entity.ContentType.MediaSubtype + entity.ContentType.Parameters;
                 requestNode.Add ("Content-Type", contentType);
-                requestNode.Add ("content", stream.ToArray ());
+
+                // Making sure content of HTTP request is MimeEntity
+                using (var stream = new MemoryStream ()) {
+                    entity.WriteTo (stream, true);
+                    requestNode.Add ("content", stream.ToArray ());
+                }
             }
+
+            // Adding all other headers
+            requestNode.AddRange (headerList);
 
             // Invoking Active Event that create HTTP request
             return context.RaiseNative(activeEventName, requestNode);
@@ -113,7 +136,8 @@ namespace p5.mime.plugins
             ApplicationContext context, 
             Node args, 
             Node result, 
-            Node decryptionKeys)
+            Node decryptionKeys,
+            Node attachmentFolder)
         {
             // Moving entire result into args
             args.AddRange (result.Children);
@@ -130,6 +154,10 @@ namespace p5.mime.plugins
                     // Making sure we pass in decryption keys, if there are any
                     if (decryptionKeys != null)
                         parseMimeNode.Add (decryptionKeys);
+
+                    // Making sure we pass in [attachment-folder], if there was one declared
+                    if (attachmentFolder != null)
+                        parseMimeNode.Add (attachmentFolder);
 
                     // Making sure pass in Content-Type header from response
                     parseMimeNode.Add ("Content-Type", idxResult["Content-Type"].Value);
