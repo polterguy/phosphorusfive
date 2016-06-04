bfe806e0ce9e32d8b489787bd83f61a331821a6c
Thomas Hansen <phosphorusfive@gmail.com>
2016-06-04 13:57:53 +0200

    foo
    bar

--- plugins/p5.lambda/keywords/Sort.cs
+++ plugins/p5.lambda/keywords/Sort.cs
@@ -3,6 +3,7 @@
  * Phosphorus Five is licensed under the terms of the MIT license, see the enclosed LICENSE file for details
  */
 
+using System;
 using System.Text;
 using System.Linq;
 using System.Collections.Generic;
@@ -30,25 +31,45 @@ namespace p5.lambda.keywords
 
                 // Creating copy of node list to insert as result of [sort] after sorting nodelist
                 var nodeList = new List<Node> (XUtil.Iterate<Node> (context, e.Args, true).Select (ix => ix.Clone ()));
-                nodeList.Sort (delegate(Node lhs, Node rhs) {
 
-                    // Cloning [sort] node, using clone for [eval] for each node in result
-                    var exeNode = e.Args.Clone ();
+                // Special case for empty [sort] node, at which case we use "default sorting", sorting by value
+                if (e.Args.Children.Count (ix => ix.Name != "") == 0) {
 
-                    // Making sure exeNode has name of [eval], and forcing children evaluation
-                    exeNode.Name = "eval";
-                    exeNode.Value = null;
+                    // Defaulting to sorting nodes by "value" casted to IComparable
+                    nodeList.Sort (delegate(Node lhs, Node rhs) {
+                        if (lhs.Value == null && rhs.Value != null)
+                            return -1;
+                        else if (lhs.Value != null && rhs.Value == null)
+                            return 1;
+                        else if (lhs.Value == null && rhs.Value == null)
+                            return 0;
 
-                    // Injecting [_lhs] and [_rhs] to sort callback
-                    exeNode.Insert (0, new Node ("_lhs", lhs));
-                    exeNode.Insert (1, new Node ("_rhs", rhs));
+                        // Assuming value somehow implements IComparable
+                        return (lhs.Value as IComparable).CompareTo (rhs.Value);
+                    });
+                } else {
 
-                    // Invoking [sort] callback
-                    context.RaiseLambda ("eval", exeNode);
+                    // We have a lambda delegate here
+                    nodeList.Sort (delegate(Node lhs, Node rhs) {
 
-                    // Retrieving value of [sort] callback, defaulting to "equals"
-                    return exeNode.Get<int> (context, 0);
-                });
+                        // Cloning [sort] node, using clone for [eval] for each node in result
+                        var exeNode = e.Args.Clone ();
+
+                        // Making sure exeNode has name of [eval], and forcing children evaluation
+                        exeNode.Name = "eval";
+                        exeNode.Value = null;
+
+                        // Injecting [_lhs] and [_rhs] to sort callback
+                        exeNode.Insert (0, new Node ("_lhs", lhs));
+                        exeNode.Insert (1, new Node ("_rhs", rhs));
+
+                        // Invoking [sort] callback
+                        context.RaiseLambda ("eval", exeNode);
+
+                        // Retrieving value of [sort] callback, defaulting to "equals"
+                        return exeNode.Get<int> (context, 0);
+                    });
+                }
 
                 // Returning sorted list to caller
                 e.Args.AddRange (nodeList);
