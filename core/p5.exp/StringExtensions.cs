2a1ca39d69481eb886445386092bba48503730db
Thomas Hansen <phosphorusfive@gmail.com>
2016-12-24 11:05:53 +0200

    Dox

--- core/p5.exp/StringExtensions.cs
+++ core/p5.exp/StringExtensions.cs
@@ -24,25 +24,51 @@
 namespace p5.exp
 {
     /// <summary>
-    ///     Contains extension methods for string, to make sure we by default have culture-awareness sane methods.
+    ///     Contains extension methods for string, to make sure we by default have culture-awareness sane methods for most common operations.
     /// </summary>
     public static class StringExtensions
     {
+        /// <summary>
+        ///     Invokes string.IndexOf with invariant culture.
+        /// </summary>
+        /// <returns>The index of the what string, if found, otherwise -1</returns>
+        /// <param name="self">The "this" parameter</param>
+        /// <param name="what">What to look for</param>
+        /// <param name="startIndex">The start index of where to start looking</param>
         public static int IndexOfEx (this string self, string what, int startIndex = 0)
         {
             return self.IndexOf (what, startIndex, System.StringComparison.InvariantCulture);
         }
 
+        /// <summary>
+        ///     Invokes string.LastIndexOf with invariant culture.
+        /// </summary>
+        /// <returns>The last index of the what string, if found, otherwise -1</returns>
+        /// <param name="self">The "this" parameter</param>
+        /// <param name="what">What to look for</param>
+        /// <param name="startIndex">The start index of where to start looking</param>
         public static int LastIndexOfEx (this string self, string what)
         {
             return self.LastIndexOf (what, System.StringComparison.InvariantCulture);
         }
 
+        /// <summary>
+        ///     Returns true if string starts with the given string.
+        /// </summary>
+        /// <returns><c>true</c>, if this starts with what, <c>false</c> otherwise.</returns>
+        /// <param name="self">The this parameter</param>
+        /// <param name="what">What to check for</param>
         public static bool StartsWithEx (this string self, string what)
         {
             return self.StartsWith (what, System.StringComparison.InvariantCulture);
         }
 
+        /// <summary>
+        ///     Returns true if string ends with the given string.
+        /// </summary>
+        /// <returns><c>true</c>, if this ends with what, <c>false</c> otherwise.</returns>
+        /// <param name="self">The this parameter</param>
+        /// <param name="what">What to check for</param>
         public static bool EndsWithEx (this string self, string what)
         {
             return self.EndsWith (what, System.StringComparison.InvariantCulture);
