338041bd9c406e20c042fda03c23e9994e058e26
Thomas Hansen <phosphorusfive@gmail.com>
2017-04-09 21:41:12 +0300

    Oops, method wasn't deducted correctly from Active Event name
    http should have been HTTP.

--- plugins/extras/p5.http/HttpRequest.cs
+++ plugins/extras/p5.http/HttpRequest.cs
@@ -96,7 +96,7 @@ namespace p5.http
             using (new ArgsRemover (args, true)) {
 
                 // Figuring out which HTTP method to use
-                string method = args.Name.Split ('.').Last ();
+                string method = args.Name.Split ('.').Last ().ToUpper ();
                 if (method.Contains ("-"))
                     method = method.Substring (0, method.IndexOfEx ("-"));
                 try
