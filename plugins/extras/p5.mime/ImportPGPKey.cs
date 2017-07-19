5d30830b80e7d8e0ea898955343b136d3ffdcc31
Thomas Hansen <phosphorusfive@gmail.com>
2017-07-19 13:24:52 +0300

    Preview enabled of PGP key
    Through Active Event that allows you to see the key’s details before
    you import it. Necessary to have user explicitly allow the key to be
    imported.

--- plugins/extras/p5.mime/ImportPGPKey.cs
+++ plugins/extras/p5.mime/ImportPGPKey.cs
@@ -42,37 +42,83 @@ namespace p5.mime
         /// </summary>
         /// <param name="context">Application Context</param>
         /// <param name="e">Active Event arguments</param>
-        [ActiveEvent (Name = "p5.crypto.import-public-pgp-key")]
-        private static void p5_crypto_import_public_pgp_key (ApplicationContext context, ActiveEventArgs e)
+        [ActiveEvent (Name = "p5.crypto.preview-public-pgp-key")]
+        private static void p5_crypto_preview_public_pgp_key (ApplicationContext context, ActiveEventArgs e)
         {
             // House cleaning
             using (new ArgsRemover (e.Args, true)) {
 
-                // Creating new GnuPG context
-                using (var ctx = new GnuPrivacyContext ()) {
+                // Looping through each public key (in ascii armored format) and importing into GnuPG database
+                foreach (var idxKey in XUtil.Iterate<string> (context, e.Args)) {
 
-                    // Looping through each public key (in ascii armored format) and importing into GnuPG database
-                    foreach (var idxKey in XUtil.Iterate<string> (context, e.Args)) {
+                    // Creating armored input stream to wrap key
+                    using (var memStream = new MemoryStream (Encoding.UTF8.GetBytes (idxKey.Replace ("\r\n", "\n")))) {
+                        using (var armored = new ArmoredInputStream (memStream)) {
+                            var keys = new PgpPublicKeyRing (armored);
 
-                        // Creating armored input stream to wrap key
-                        using (var memStream = new MemoryStream (Encoding.UTF8.GetBytes (idxKey.Replace ("\r\n", "\n")))) {
-                            using (var armored = new ArmoredInputStream (memStream)) {
-                                var key = new PgpPublicKeyRing (armored);
-                                ctx.Import (key);
-                                e.Args.Add (BitConverter.ToString (key.GetPublicKey ().GetFingerprint ()).Replace ("-", ""));
-                            }
+                            // Now returning key details to caller.
+                            foreach (PgpPublicKey key in keys.GetPublicKeys ()) {
+                                var node = e.Args.Add (BitConverter.ToString (key.GetFingerprint ()).Replace ("-", "")).LastChild;
+								node.Add("id", ((int)key.KeyId).ToString("X"));
+								node.Add("algorithm", key.Algorithm.ToString());
+								node.Add("strength", key.BitStrength);
+								node.Add("creation-time", key.CreationTime);
+								node.Add("is-encryption-key", key.IsEncryptionKey);
+								node.Add("is-master-key", key.IsMasterKey);
+								node.Add("is-revoked", key.IsRevoked());
+								node.Add("version", key.Version);
+								DateTime expires = key.CreationTime.AddSeconds(key.GetValidSeconds());
+								node.Add("expires", expires);
+								foreach (var idxUserId in key.GetUserIds()) {
+									if (idxUserId is string)
+										node.FindOrInsert("user-ids").Add("", idxUserId);
+								}
+								foreach (PgpSignature signature in key.GetSignatures()) {
+									node.FindOrInsert("signed-by").Add(((int)signature.KeyId).ToString("X"), signature.CreationTime);
+								}
+							}  
                         }
                     }
                 }
             }
         }
 
-        /// <summary>
-        ///     Imports the supplied key(s) into GnuPG database
-        /// </summary>
-        /// <param name="context">Application Context</param>
-        /// <param name="e">Active Event arguments</param>
-        [ActiveEvent (Name = "p5.crypto.import-private-pgp-key")]
+		/// <summary>
+		///     Imports the supplied key(s) into GnuPG database
+		/// </summary>
+		/// <param name="context">Application Context</param>
+		/// <param name="e">Active Event arguments</param>
+		[ActiveEvent(Name = "p5.crypto.import-public-pgp-key")]
+		private static void p5_crypto_import_public_pgp_key(ApplicationContext context, ActiveEventArgs e)
+		{
+			// House cleaning
+			using (new ArgsRemover (e.Args, true)) {
+
+				// Creating new GnuPG context
+				using (var ctx = new GnuPrivacyContext ()) {
+
+					// Looping through each public key (in ascii armored format) and importing into GnuPG database
+					foreach (var idxKey in XUtil.Iterate<string> (context, e.Args)) {
+
+						// Creating armored input stream to wrap key
+						using (var memStream = new MemoryStream (Encoding.UTF8.GetBytes (idxKey.Replace ("\r\n", "\n")))) {
+							using (var armored = new ArmoredInputStream (memStream)) {
+								var key = new PgpPublicKeyRing (armored);
+								ctx.Import (key);
+								e.Args.Add (BitConverter.ToString (key.GetPublicKey().GetFingerprint()).Replace("-", ""));
+							}
+						}
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		///     Imports the supplied key(s) into GnuPG database
+		/// </summary>
+		/// <param name="context">Application Context</param>
+		/// <param name="e">Active Event arguments</param>
+		[ActiveEvent (Name = "p5.crypto.import-private-pgp-key")]
         private static void p5_crypto_import_private_pgp_key (ApplicationContext context, ActiveEventArgs e)
         {
             // House cleaning
