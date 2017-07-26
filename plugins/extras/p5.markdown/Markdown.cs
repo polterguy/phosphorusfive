001bcbb82327fe22171b53c1942ebef1a8e05705
Thomas Hansen <phosphorusfive@gmail.com>
2017-07-26 20:09:35 +0300

    Support for unrolling path in [root-url]

--- plugins/extras/p5.markdown/Markdown.cs
+++ plugins/extras/p5.markdown/Markdown.cs
@@ -48,16 +48,19 @@ namespace p5.markdown
                 var md = XUtil.Single<string> (context, e.Args);
 
                 // Making sure we correctly resolve URLs, if user specified a [root-url] argument.
-                var root = e.Args.GetExChildValue ("root-url", context, "").TrimEnd ('/');
+                var root = e.Args.GetExChildValue ("root-url", context, "");
                 CommonMarkSettings settings = CommonMarkSettings.Default;
 				if (root != "") {
+
+                    // Unrolling path.
+                    root = context.RaiseEvent ("p5.io.unroll-path", new Node ("", root)).Get<string>(context);
 
                     // To make sure we don't change global settings.
                     settings = settings.Clone ();
                     settings.UriResolver = delegate (string arg) {
                         if (arg.StartsWithEx ("http://") || arg.StartsWithEx ("https://"))
                             return arg;
-                        return root + "/" + arg.TrimStart ('/');
+                        return root + arg.TrimStart ('/');
                     };
                 }
 
