74686cde267a81c54184a354cc741109bab0bce1
Thomas Hansen <phosphorusfive@gmail.com>
2017-10-05 11:05:03 +0300

    Formatting

--- plugins/p5.config/Properties/AssemblyInfo.cs
+++ plugins/p5.config/Properties/AssemblyInfo.cs
@@ -27,22 +27,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("p5.config")]
-[assembly: AssemblyDescription("Configuration Active Events")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("p5.config")]
-[assembly: AssemblyCopyright("Copyright © Thomas Hansen 2016 - 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle ("p5.config")]
+[assembly: AssemblyDescription ("Configuration Active Events")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("p5.config")]
+[assembly: AssemblyCopyright ("Copyright © Thomas Hansen 2016 - 2017")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible (false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("66d68ce5-ff39-4ff2-8cf4-cd8a0839cb9f")]
+[assembly: Guid ("66d68ce5-ff39-4ff2-8cf4-cd8a0839cb9f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -51,5 +51,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
