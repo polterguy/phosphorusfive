971a7bc269beaca90f020ecc6a206e04097e8844
Thomas Hansen <phosphorusfive@gmail.com>
2017-12-10 11:30:04 +0200

    Started removal of mod_mono_auto
    Since it doesn’t work anyways with complex folder structures

--- install.sh
+++ install.sh
@@ -15,6 +15,11 @@ echo "GNU General Public License for more details."
 echo "================================================================================"
 echo ""
 
+# Making sure we update and upgrade our server
+apt-get update
+apt-get upgrade
+apt-get dist-upgrade
+
 # Download P5, and showing SHA1 to user, asking if he wants to proceed.
 wget https://github.com/polterguy/phosphorusfive/releases/download/v5.7/binaries.zip
 sha1sum binaries.zip
@@ -31,6 +36,9 @@ fi
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password SomeRandomPassword'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password SomeRandomPassword'
 apt-get --assume-yes install apache2 mysql-server libapache2-mod-mono unzip gnupg2
+
+# Disabling mod_mono_auto to make sure we create an "advanced" configuration for mod_mono
+a2dismod mod_mono_auto
 
 # Removing any old files.
 # Notice, we don't remove "/common" and "/users" here.
@@ -59,52 +67,128 @@ rm -f -r p5
 sed -i 's/User Id=root;/User Id=root;password=SomeRandomPassword;/g' /var/www/html/web.config
 
 # Making GnuPG folder for Apache process.
-mkdir /var/www/.gnupg
+    
 
 # Giving ownership (recursively) to Apache user for entire folder.
 # Necessary since P5 will create and modify its own file structure.
 chown -R www-data:www-data /var/www
 
-# Configuring mod_mono
-echo "MonoAutoApplication enabled
-AddType application/x-asp-net .aspx
-AddType application/x-asp-net .asmx
-AddType application/x-asp-net .ashx
-AddType application/x-asp-net .asax
-AddType application/x-asp-net .ascx
-AddType application/x-asp-net .soap
-AddType application/x-asp-net .rem
-AddType application/x-asp-net .axd
-AddType application/x-asp-net .cs
-AddType application/x-asp-net .config
-AddType application/x-asp-net .dll
-DirectoryIndex index.aspx
+# Configuring mod_mono app
+echo "
+<apps>
+        <web-application>
+                <name>P5</name>
+                <vpath>/</vpath>
+                <path>/var/www/html</path>
+                <vhost>example.com</vhost>
+        </web-application>
+</apps>
+" > /etc/mono-server4/p5.webapp
+
+# Configuring apache to accept requests for ASP.NET, and route them into Mono
+echo "
+
+
+# Phosphorus Five configurations
+# These parts was added as a part of the install.sh script while installing Phosphorus Five.
+
+MonoAutoApplication disabled
+AddHandler mono .aspx .axd .config
+MonoApplications \"/:/var/www/html\"
 DirectoryIndex Default.aspx
-DirectoryIndex default.aspx
-<FilesMatch \"^[^\.]+$\">
-    ForceType application/x-asp-net
-</FilesMatch>
-<Files ~ \"\.hl\">
+
+# Turning OFF all options, and making sure they're impossible to override using .htaccess files, to be absolutely certain.
+<Location />
+    Options None
+    AllowOverride None
+</Location>
+
+# Making sure Mono handles all requests that doesn't have an extension, to make our URL rewriting logic work.
+<Location ~ \"^[^\.]*$\">
+    SetHandler mono
+</Location>
+
+# Making sure we never serve Hyperlambda files.
+<Files ~ \"\.hl$\">
     Order allow,deny
     Deny from all
 </Files>
-<Location \"/users/[^/]/documents/private/\">
+
+# Making user's private documents just that.
+<Location ~ \"/users/[^/]/documents/private/\">
     Order allow,deny
     Deny from all
 </Location>
-<Location \"/users/[^/]/temp/\">
+
+# Making all temp files for users inaccessible.
+<Location ~ \"/users/[^/]/temp/\">
     Order allow,deny
     Deny from all
 </Location>
-<Location \"/common/documents/private/\">
+
+# Making all common private files just that.
+<Location ~ \"/common/documents/private/\">
     Order allow,deny
     Deny from all
 </Location>
-<Location \"/common/temp/\">
+
+# Making all common temp files private
+<Location ~ \"/common/temp/\">
     Order allow,deny
     Deny from all
 </Location>
-" > /etc/apache2/mods-enabled/mod_mono_auto.conf
+
+
+# Phosphorus Five, further tightening server, to make it more secure.
+
+# Disabling SSL2.0 and 3.0
+SSLProtocol -ALL +TLSv1
+
+# Disabling all handshakes on lower cipher suites
+SSLCipherSuite ALL:!aNULL:!ADH:!eNULL:!LOW:!EXP:RC4+RSA:+HIGH:+MEDIUM
+
+# Turning OFF tracing
+TraceEnable off
+
+# Turning OFF server signature and token
+ServerSignature Off
+ServerTokens Prod
+
+# Turning OFF ETags, to avoid information leaking.
+FileETag None
+
+# Making sure server never returns a cookie which is not secure and HTTP only.
+Header edit Set-Cookie ^(.*)$ \$1;HttpOnly;Secure
+
+# Turning OFF mod_security for WebResource.axd
+<Location \"/WebResource.axd\">
+    <IfModule security2_module>
+        SecRuleEngine Off
+    </IfModule>
+</Location>
+
+# Turning OFF mod_security for folders where we need more slack.
+<Location ~ \"/[^/]+/settings\">
+    <IfModule security2_module>
+        SecRuleEngine Off
+    </IfModule>
+</Location>
+<Location \"/hypereval\">
+    <IfModule security2_module>
+        SecRuleEngine Off
+    </IfModule>
+</Location>
+" >> /etc/apache2/apache2.conf
+
+# Enabling headers modules in apache
+a2enmod headers
+
+# Installing SSL keys
+apt-get install software-properties-common
+add-apt-repository ppa:certbot/certbot
+apt-get update
+apt-get install python-certbot-apache
+sudo certbot --apache
 
 # Restarting Apache
 service apache2 restart
